7b45ed629cd34300bd5dc7fe3ba5a1a1a7724423
Angel-Perdomo <pastorangel1880@gmail.com>
2022-03-01 15:58:23 -0600

    Error al actualizar la imagen en editar

--- Cafeteria/Cafeteria.WebAdmin/Controllers/ClientesController.cs
+++ Cafeteria/Cafeteria.WebAdmin/Controllers/ClientesController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Cafeteria.WebAdmin.Controllers
 {
-    public class ClientesController : Controller
+    public class ClientesController : Controller 
     {
         ClientesBL _clientesBL;
 
